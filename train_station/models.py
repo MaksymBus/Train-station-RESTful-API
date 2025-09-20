59e72fda512c0be76c4f33a893d3a522c4c31273
Maksym Buslenko <maksbusl@gmail.com>
2025-09-20 18:24:41 +0300

    add train_image_file_path function, Route, Journey, Order, Ticket models

--- train_station/models.py
+++ train_station/models.py
@@ -1,4 +1,10 @@
+import os
+import uuid
+
 from django.db import models
+from django.utils.text import slugify
+
+
 class TrainType(models.Model):
     name = models.CharField(
         max_length=255,
@@ -9,4 +15,157 @@ class TrainType(models.Model):
         return self.name
 
 
-# Create your models here.
+def train_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/trains/", filename)
+
+
+
+    class Meta:
+        ordering = ["name",]
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station,
+        related_name="routes",
+        on_delete=models.CASCADE
+    )
+    destination = models.ForeignKey(
+        Station,
+        related_name="routes",
+        on_delete=models.CASCADE
+    )
+    distance = models.IntegerField()
+
+    class Meta:
+        constrains = [
+            models.UniqueConstraint(
+                fields=["source", "destination"],
+                name="unique_source_destination"
+            )
+        ]
+
+    def clean(self):
+        if self.source == self.destination:
+            raise ValidationError("Source and destination can't be the same")
+
+    def save(
+        self,
+        *args,
+        **kwargs
+    ):
+        self.clean()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.source} - {self.destination}"
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(Route, related_name="journeys", on_delete=models.CASCADE)
+    train = models.ForeignKey(Train, related_name="journeys", on_delete=models.CASCADE)
+    crew = models.ManyToManyField(Crew, blank=True, related_name="journeys")
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def clean(self):
+        if self.departure_time >= self.arrival_time:
+            raise ValidationError("Departure time can't be more or equal to arrival time")
+
+    def save(
+        self,
+        *args,
+        **kwargs
+    ):
+        self.clean()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return (f"Route: {self.route.source} - {self.route.destination}, "
+                f"Train: {self.train.name}")
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        get_user_model(),
+        related_name="orders",
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return str(self.created_at)
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(
+        Journey,
+        related_name="tickets",
+        on_delete=models.CASCADE
+    )
+    order = models.ForeignKey(
+        Order,
+        related_name="tickets",
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        unique_together = ("journey", "cargo", "seat")
+        ordering = ["cargo", "seat"]
+
+    @staticmethod
+    def validate_ticket(
+            cargo: int,
+            seat: int,
+            train: Train,
+            error_to_raise: Type[ValidationError]
+    ) -> None:
+        if not (1 <= cargo <= train.cargo_num):
+            raise error_to_raise(
+                {
+                    "cargo": f"Cargo number must be in available range: "
+                             f"(1, {train.cargo_num})"
+                }
+            )
+        if not (1 <= seat <= train.places_in_cargo):
+            raise error_to_raise(
+                {
+                    "seat": f"Seat number must be in available range: "
+                            f"(1, {train.places_in_cargo})"
+                }
+            )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.cargo,
+            self.seat,
+            self.journey.train,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        *args,
+        force_insert = False,
+        force_update = False,
+        using = None,
+        update_fields = None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return f"{str(self.journey)} (cargo: {self.cargo}, seat: {self.seat})"
