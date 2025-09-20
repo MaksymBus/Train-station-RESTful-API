0712a9fc2d13be52c4413e7531a5cca3499f58c2
Maksym Buslenko <maksbusl@gmail.com>
2025-09-20 19:37:34 +0300

    create StationSerializer

--- train_station/serializers.py
+++ train_station/serializers.py
@@ -2,12 +2,18 @@ from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from train_station.models import TrainType
+from train_station.models import TrainType, Station
 
 
 class TrainTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrainType
         fields = ("id", "name")
+
+
+class StationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Station
+        fields = ("id", "name", "latitude", "longitude")
 
 
