8642159abd32faf3435dfa12cbded530b2fc52ae
Maksym Buslenko <maksbusl@gmail.com>
2025-09-20 20:06:22 +0300

    create CrewSerializer and TrainSerializer

--- train_station/serializers.py
+++ train_station/serializers.py
@@ -2,7 +2,16 @@ from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from train_station.models import TrainType, Station
+from train_station.models import (
+    TrainType,
+    Station,
+    Crew,
+    Journey,
+    Order,
+    Route,
+    Train,
+    Ticket
+)
 
 
 class TrainTypeSerializer(serializers.ModelSerializer):
@@ -14,6 +23,34 @@ class TrainTypeSerializer(serializers.ModelSerializer):
 class StationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Station
-        fields = ("id", "name", "latitude", "longitude")
+        fields = (
+            "id",
+            "name",
+            "latitude",
+            "longitude"
+        )
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "full_name"
+        )
+
+
+class TrainSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Train
+        fields = (
+            "id",
+            "name",
+            "cargo_num",
+            "places_in_cargo",
+            "train_type"
+        )
 
 
