6e79170a733b540cbea65ed0181d6024548f2f3f
Maksym Buslenko <maksbusl@gmail.com>
2025-09-20 18:25:41 +0300

    create Train model

--- train_station/models.py
+++ train_station/models.py
@@ -22,6 +22,28 @@ def train_image_file_path(instance, filename):
     return os.path.join("uploads/trains/", filename)
 
 
+class Train(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(
+        TrainType,
+        related_name="trains",
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(
+        null=True,
+        upload_to=train_image_file_path
+    )
+
+    class Meta:
+        ordering = ["name",]
+
+    def __str__(self):
+        return self.name
+
 
     class Meta:
         ordering = ["name",]
