0cc9d97fe560638bdb0a9be2f304a95b0ac62807
Maksym Buslenko <maksbusl@gmail.com>
2025-09-20 18:22:50 +0300

    create TrainType model

--- train_station/models.py
+++ train_station/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+class TrainType(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
