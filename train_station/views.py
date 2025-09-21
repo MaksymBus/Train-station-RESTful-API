f9e4fd115605540ed773d2f7637c553331201dcf
Maksym Buslenko <maksbusl@gmail.com>
2025-09-21 20:12:46 +0300

    create TrainTypeViewSet, CrewViewSet, StationViewSer

--- train_station/views.py
+++ train_station/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from train_station.models import (
+    TrainType,
+    Crew,
+    Journey,
+    Train,
+    Route,
+    Station,
+    Ticket,
+    Order
+)
+from train_station.serializers import (
+    TrainTypeSerializer,
+    CrewSerializer,
+    StationSerializer,
+)
+
+
+class TrainTypeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class CrewViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class StationViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
