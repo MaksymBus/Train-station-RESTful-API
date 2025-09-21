9d0e19183f13f2f0e154dee414685635d59e86ea
Maksym Buslenko <maksbusl@gmail.com>
2025-09-21 12:18:05 +0300

    create OrderSerializer and OrderListSerializer

--- train_station/serializers.py
+++ train_station/serializers.py
@@ -226,3 +226,31 @@ class JourneyDetailSerializer(JourneySerializer):
             "arrival_time",
             "taken_places"
         )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(
+        many=True,
+        read_only=False,
+        allow_empty=False
+    )
+
+    class Meta:
+        model = Order
+        fields = (
+            "id",
+            "tickets",
+            "created_at"
+        )
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
+
+
+class OrderListSerializer(OrderSerializer):
+    tickets = TicketListSerializer(many=True, read_only=True)
