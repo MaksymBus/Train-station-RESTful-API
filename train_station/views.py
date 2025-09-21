6d9ad7b76c739d19d97c8457203a8a963ff774de
Maksym Buslenko <maksbusl@gmail.com>
2025-09-21 21:52:10 +0300

    create RouteViewSet, JourneyViewSet

--- train_station/views.py
+++ train_station/views.py
@@ -1,6 +1,7 @@
+from django.db.models import F, Count
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema, OpenApiParameter
-from rest_framework import mixins, status
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
@@ -20,7 +21,8 @@ from train_station.serializers import (
     TrainTypeSerializer,
     CrewSerializer,
     StationSerializer, TrainSerializer, TrainListSerializer, TrainDetailSerializer, TrainImageSerializer,
-    RouteListSerializer, RouteDetailSerializer, RouteSerializer,
+    RouteListSerializer, RouteDetailSerializer, RouteSerializer, JourneyListSerializer, JourneyDetailSerializer,
+    JourneySerializer,
 )
 
 
@@ -116,6 +118,123 @@ class TrainViewSet(
                         "Filter by name of Train "
                         "(ex. ?name=Intercity)"
                 ),
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+class RouteViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Route.objects.select_related("source", "destination")
+
+    def get_queryset(self):
+        source_id_str = self.request.query_params.get("source")
+        destination_id_str = self.request.query_params.get("destination")
+
+        queryset = self.queryset
+
+        if source_id_str:
+            queryset = queryset.filter(source_id=int(source_id_str))
+
+        if destination_id_str:
+            queryset = queryset.filter(destination_id=int(destination_id_str))
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+
+        return RouteSerializer
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "source",
+                type=OpenApiTypes.INT,
+                description="Filter by source id (ex. ?source=2)",
+            ),
+            OpenApiParameter(
+                "destination",
+                type=OpenApiTypes.INT,
+                description="Filter by destination id (ex. ?destination=2)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Journey.objects.select_related("route", "train")
+        .prefetch_related("crew")
+        .annotate(
+            tickets_available=(
+                    F("train__cargo_num") * F("train__places_in_cargo")
+                    - Count("tickets")
+            )
+        )
+    )
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        train_id_str = self.request.query_params.get("train")
+        route_id_str = self.request.query_params.get("route")
+        crew = self.request.query_params.get("crew")
+
+        queryset = self.queryset
+
+        if crew:
+            crew_ids = self._params_to_ints(crew)
+            queryset = queryset.filter(crew__id__in=crew_ids)
+
+        if train_id_str:
+            queryset = queryset.filter(train_id=int(train_id_str))
+
+        if route_id_str:
+            queryset = queryset.filter(route_id=int(route_id_str))
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return JourneyListSerializer
+
+        if self.action == "retrieve":
+            return JourneyDetailSerializer
+
+        return JourneySerializer
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "crew",
+                type={"type": "list", "items": {"type": "number"}},
+                description="Filter by crew id (ex. ?crew=2,5)",
+            ),
+            OpenApiParameter(
+                "train",
+                type=OpenApiTypes.INT,
+                description="Filter by train id (ex. ?train=2)",
+            ),
+            OpenApiParameter(
+                "route",
+                type=OpenApiTypes.INT,
+                description="Filter by route id (ex. ?route=2)",
             ),
         ]
     )
