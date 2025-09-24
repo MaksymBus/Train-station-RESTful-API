0ab595e4c749d3aab9b76deb555bf3beed9eeb68
Maksym Buslenko <maksbusl@gmail.com>
2025-09-24 10:50:23 +0300

    update settings with env variables SECRET_KEY and DEBUG

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -9,21 +9,24 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^!fwb=^no1l&0%yyw@2r19o@f#=pz*0e!niowim)9q630^0+9c"
+SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
